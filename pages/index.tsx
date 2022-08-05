fffb95a72ab8a4f799a26b37f34ce131eb7c4de9
Cassidy Smith <cassidysmith319@gmail.com>
2022-08-04 19:30:53 -0500

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,29 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z } from '../components/Letter'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Blood Letter</title>
+      </Head>
+      
+
+      <main className={styles.main}>
+        <h1>Blood Letter</h1>
+
+        <E />
+        <S />
+        <L />
+        <W />
+        <P />
+        <J />
+
+      </main>
+    </div>
+  )
+}
+
+export default Home
