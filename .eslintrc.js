d7122a5ac0a9160bbcb0fa6891b9a432c5f37f58
Cassidy Smith <cassidysmith319@gmail.com>
2022-08-06 01:17:58 -0500

    set up prettier and eslint code formatting

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'standard',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', '@typescript-eslint'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+}
