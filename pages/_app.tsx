e44a697547f8c901c6e84bc315fbcc4f688d5acd
Cassidy Smith <cassidysmith319@gmail.com>
2022-08-21 03:38:07 -0500

    fix FOUC

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,9 @@ import type { AppProps } from 'next/app'
 import { globalStyles } from '../styles/globals'
 import { GameContextProvider } from '../context/GameContext'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  globalStyles()
+globalStyles()
 
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <GameContextProvider>
       <Component {...pageProps} />
